8aa3bd559d9a68724ec710deb88457b3aacc85fe
unknown <gabrielpordeus@gmail.com>
2020-03-23 19:10:54 -0300

    build

---
+++ build/precache-manifest.ab768c7084141522532fdb71d3b5e6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0d371f720086e5dfc6a0b8bbf980b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2af0a8dfc687e17226a",
+    "url": "/static/css/main.cacbc0fe.chunk.css"
+  },
+  {
+    "revision": "2d750a00b3749ed2cbbc",
+    "url": "/static/js/2.f835f102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f835f102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2af0a8dfc687e17226a",
+    "url": "/static/js/main.37413b60.chunk.js"
+  },
+  {
+    "revision": "7dee0c0cd78f2d82bd46",
+    "url": "/static/js/runtime-main.5e829126.js"
+  }
+]);
--- frontend/prime-checker-frontend/src/components/ToggleMessage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function ToggleMessage(props) {
-    if (props.show) return <p className={props.type}><b>{props.message}</b></p>
-    return null
-}
-
-export default ToggleMessage
